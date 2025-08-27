907bdfc7feccc9ce489118cbc8aab43ce65401ba
Mayara Froes <froeselias721@gmail.com>
2025-08-27 15:49:20 -0400

    Adicionando o Loop For

--- jogo_adivinhacao.c
+++ jogo_adivinhacao.c
@@ -10,25 +10,28 @@ int main() {
     int numerosecreto = 42; // declarando a variavel e o valor da variavel
     int chute;
 
-    printf("Qual o seu chute? "); // pedindo o chute do usuario
-    scanf("%d", &chute); // lendo o chute do usuário, '&' indica o endereço da memória onde o valor será armazenado
-    printf("\nSeu chute foi %d\n", chute);
+    for (int i = 1; i <= 3; i++) { // loop para 3 tentativas
+        printf("Tentativa %d de 3\n", i); // imprimindo o número da tentativa atual
+        printf("Qual o seu chute? "); // pedindo o chute do usuario
+        scanf("%d", &chute); // lendo o chute do usuário, '&' indica o endereço da memória onde o valor será armazenado
+        printf("\nSeu chute foi %d\n", chute);
 
 
-    int acertou = (chute == numerosecreto); // declarando a variavel e atribuindo o valor booleano (1 ou 0)
-    
-    // Definindo a lógica do jogo
-    // utilizando a variavel 'acertou' para verificar se o chute é igual ao número secreto
-    if (acertou) {
-        printf("\nParabens! Voce acertou!\n");
-        printf("Jogue de novo, voce e um bom jogador!");
-    } else { // se o chute for diferente do número secreto
-        int maior = (chute > numerosecreto);
-        if (maior) {
-            printf("Seu chute foi maior que o umero secreto!\n");
-        } else {
-            printf("Seu chute foi menor que o numero secreto!\n");
+        int acertou = (chute == numerosecreto); // declarando a variavel e atribuindo o valor booleano (1 ou 0)
+        
+        // Definindo a lógica do jogo
+        // utilizando a variavel 'acertou' para verificar se o chute é igual ao número secreto
+        if (acertou) {
+            printf("\nParabens! Voce acertou!\n");
+            printf("Jogue de novo, voce e um bom jogador!");
+        } else { // se o chute for diferente do número secreto
+            int maior = (chute > numerosecreto);
+            if (maior) {
+                printf("Seu chute foi maior que o umero secreto!\n");
+            } else {
+                printf("Seu chute foi menor que o numero secreto!\n");
+            }
         }
     }
-    
+    printf("Fim de jogo!\n");
 }
