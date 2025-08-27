db260e9ec033d5685447649ec7744bbfc7493f98
Mayara Froes <froeselias721@gmail.com>
2025-08-27 12:04:29 -0400

    Aula 1 - Func Printf e Scanf

--- jogo_adivinhacao.c
+++ jogo_adivinhacao.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+// funão main: ponto de entrada do programa
 int main() {
-    printf("Bem vindo ao jogo de adivinhação!\n");
+    // imprime o cabeçalho do jogo
+    printf("*************************************\n");
+    printf("* Bem vindo ao jogo de adivinhacao! *\n");
+    printf("*************************************\n\n");
+
+    int numerosecreto = 42; // declarando a variavel e o valor da variavel
+    int chute;
+
+    printf("Qual o seu chute? "); // pedindo o chute do usuario
+    scanf("%d", &chute); // lendo o chute do usuário, '&' indica o endereço da memória onde o valor será armazenado
+    printf("\nSeu chute foi %d", chute);
 }
